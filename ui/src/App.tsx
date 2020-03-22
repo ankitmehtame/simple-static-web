8ba0ce54e0aaa8ff2a5889fcf2be848297452b1e
Priyanka Mehta <GAC71@ad.zalora-ops.com>
2020-03-22 09:13:46 +0800

    Initial React app

---
+++ ui/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const CenterAlignDiv = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+function App() {
+  return (
+    <CenterAlignDiv>
+      <AppHeader>
+        <p>
+          Cool Home Page
+        </p>
+      </AppHeader>
+    </CenterAlignDiv>
+  );
+}
+
+export default App;
