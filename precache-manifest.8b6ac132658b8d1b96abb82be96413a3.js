d89eab13dedc7dd24b77848845915ce5dc5bf5c9
Priyanka Mehta <GAC71@ad.zalora-ops.com>
2020-03-22 09:32:24 +0800

    Updates

---
+++ precache-manifest.8b6ac132658b8d1b96abb82be96413a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853dd32acc06add354e1a12b95dbeffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632d24def660a9f596f6",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6684eb219f0d62e2db9a",
+    "url": "/static/js/2.47ea3c5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47ea3c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632d24def660a9f596f6",
+    "url": "/static/js/main.a4360d0b.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  }
+]);
