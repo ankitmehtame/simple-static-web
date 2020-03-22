b587035e7510f72c62fefdd06e6e163b2352f24e
ankitmehtame <ankitmehtame@gmail.com>
2020-03-22 13:46:44 +0800

    Add nav

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,31 +1,68 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import { HashRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 
 const CenterAlignDiv = styled.div`
   text-align: center;
 `
 
 const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`
+
+const Nav = styled.nav`
+  a {
+    color: whitesmoke;
+    font-size: 75%;
+  }
+`
+
+const AppContainer = styled.div`
+  background-color: #282c34;
+  height: 100vh;
   font-size: calc(10px + 2vmin);
   color: white;
 `
 
 function App() {
   return (
-    <CenterAlignDiv>
-      <AppHeader>
-        <p>
-          Cool Home Page
-        </p>
-      </AppHeader>
-    </CenterAlignDiv>
+    <Router>
+      <AppContainer>
+        <Nav>
+          <Link to='/home'>Home</Link>
+          &nbsp;&nbsp;
+          <Link to='/other'>Other</Link>
+        </Nav>
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to='/home' />
+          </Route>
+          <Route path="/home">
+            <CenterAlignDiv>
+              <AppHeader>
+                <p>
+                  Cool Home Page
+                </p>
+              </AppHeader>
+            </CenterAlignDiv>
+          </Route>
+          <Route path="/other">
+            <AppHeader>
+              The Other Page
+          </AppHeader>
+          </Route>
+          <Route>
+            <AppHeader>
+              Not found
+          </AppHeader>
+          </Route>
+        </Switch>
+      </AppContainer>
+    </Router>
   );
 }
 
